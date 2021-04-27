478c163166473aac285e98e664c8034f714906e6
Simo <s77555@beuth-hochschule.de>
2021-04-27 11:45:58 +0200

    Versuch 2

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 1289
+SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Lost Knight')
 
+x = 200
+y = 200
+img = pygame.image.load('Bilder\Gegner.PNG')
+rect = img.get_rect()
+rect.center = (x,y)
+
 run = True
 while run:
+
+    screen.blit(img,rect)
+
     for event in pygame.event.get():
         #quit game
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
 
 pygame.quit()
