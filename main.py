fe132a3d52aba38feddd756dac5b8b0c98427395
Simo <s77555@beuth-hochschule.de>
2021-04-27 12:28:49 +0200

    Versuch 2

--- main.py
+++ main.py
@@ -8,16 +8,28 @@ SCREEN_HEIGHT = 720
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Lost Knight')
 
-x = 200
-y = 200
-img = pygame.image.load('Bilder/Hero Knight/Sprites/HeroKnight/Idle/HeroKnight_Idle_0.png')
-rect = img.get_rect()
-rect.center = (x,y)
+
+class Figur(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        bild = pygame.image.load('Bilder/Hero Knight/Sprites/HeroKnight/Idle/HeroKnight_Idle_0.png')
+        self.Bilder = pygame.transform.scale(bild, (int(bild.get_width() * scale), int(bild.get_height() * scale)))
+        self.rect = self.Bilder.get_rect()
+        self.rect.center = (x,y)
+    def draw(self):
+        screen.blit(self.Bilder, self.rect)
+
+
+
+spieler  = Figur(200, 200, 2)
+spieler2 = Figur(400, 200, 2)
 
 run = True
 while run:
 
-    screen.blit(img,rect)
+    spieler.draw()
+    spieler2.draw()
+
 
     for event in pygame.event.get():
         #quit game
