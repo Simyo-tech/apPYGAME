2db6b5d1729e33d31768e86c50f8dea692a3a2d6
Simo <s77555@beuth-hochschule.de>
2021-04-27 11:18:33 +0200

    Versuch 2

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Shooter')
+pygame.display.set_caption('Lost Knight')
+
+run = True
+while run:
+    for event in pygame.event.get():
+        #quit game
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
