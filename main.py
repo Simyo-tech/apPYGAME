267880998eab151973e5294208a548eb546eb2bd
eddy <elbartoberlinlps@gmail.com>
2021-04-27 11:27:13 +0200

    Angepasste Auflösung

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Lost Knight')
