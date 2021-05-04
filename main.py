1211068147e78f9470076cda53c1d54c9454fd96
Simo <s77555@beuth-hochschule.de>
2021-05-04 11:25:57 +0200

    Animation-Versuch 1

--- main.py
+++ main.py
@@ -27,8 +27,15 @@ class Figur(pygame.sprite.Sprite):
     def __init__(self, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
         self.speed = speed
-        bild = pygame.image.load('Bilder/Hero Knight/Sprites/HeroKnight/Idle/HeroKnight_Idle_0.png')
-        self.Bilder = pygame.transform.scale(bild, (int(bild.get_width() * scale), int(bild.get_height() * scale)))
+        self.direction = 1
+        self.flip = False
+        self.animation_list = []
+        self.index = 0
+        for i in range(7):
+            bild = pygame.image.load('Bilder/Hero Knight/Sprites/HeroKnight/Idle/{i}.png')
+            bild = pygame.transform.scale(bild, (int(bild.get_width() * scale), int(bild.get_height() * scale)))
+            self.animation_list.append(bild)
+        self.Bilder = self.animation_list[self.index]
         self.rect = self.Bilder.get_rect()
         self.rect.center = (x,y)
 
@@ -38,15 +45,19 @@ class Figur(pygame.sprite.Sprite):
 
         if l_links:
             dx = -self.speed
+            self.flip = True
+            self.direction = -1
         if l_rechts:
             dx = self.speed
+            self.flip = False
+            self.direction = 1
 
         self.rect.x += dx
         self.rect.y += dy
 
 
     def draw(self):
-        screen.blit(self.Bilder, self.rect)
+        screen.blit(pygame.transform.flip(self.Bilder, self.flip, False), self.rect)
 
 
 
