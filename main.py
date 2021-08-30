06dd8014ca5096d17214657bc6b3e079e40389e9
eddy <elbartoberlinlps@gmail.com>
2021-08-30 13:45:54 +0200

    figuren können kämpfen und sterben/ restart eingefügt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import sys
 
 pygame.init()
 
@@ -17,8 +18,11 @@ FPS = 120
 GRAVITATION = 0.75
 
 #Spieler Aktion-variablen
+main = False
 l_links = False
 l_rechts = False
+g_links = False
+g_rechts = False
 
 #Farben Definitionen
 BG = (240, 255, 255)
@@ -68,40 +72,76 @@ class Figur(pygame.sprite.Sprite):
         self.rect = self.Bilder.get_rect()
         self.rect.center = (x,y)
 
-    def bewegen(self, l_links, l_rechts):
+    def spieler_bewegen(self, l_links, l_rechts):
         dx = 0
         dy = 0
 
-        if self.sprung == True and self.in_luft == False:
-            self.s_hoehe_y = -15
-            self.sprung = False
-            self.in_luft = True
+        if spieler.sprung == True and spieler.in_luft == False:
+            spieler.s_hoehe_y = -15
+            spieler.sprung = False
+            spieler.in_luft = True
 
         #Gravitation einfügen
-        self.s_hoehe_y += GRAVITATION
-        if self.s_hoehe_y > 15:
-            self.s_hoehe_y
-        dy += self.s_hoehe_y
+        spieler.s_hoehe_y += GRAVITATION
+        if spieler.s_hoehe_y > 15:
+            spieler.s_hoehe_y
+        dy += spieler.s_hoehe_y
 
         #Kollision checken mit Boden checken
-        if self.rect.bottom + dy > 500:
-            dy = 500 - self.rect.bottom
-            self.in_luft = False
+        if spieler.rect.bottom + dy > 500:
+            dy = 500 - spieler.rect.bottom
+            spieler.in_luft = False
 
-        if self.angriff == True and self.bild_index == 5:
-            self.angriff = False
+        if spieler.angriff == True and spieler.bild_index == 5:
+            spieler.angriff = False
 
         if l_links:
-            dx = -self.speed
-            self.drehen = True
-            self.richtung = -1
+            dx = -spieler.speed
+            spieler.drehen = True
+            spieler.richtung = -1
         if l_rechts:
-            dx = self.speed
-            self.drehen = False
-            self.richtung = 1
+            dx = spieler.speed
+            spieler.drehen = False
+            spieler.richtung = 1
 
-        self.rect.x += dx
-        self.rect.y += dy
+        spieler.rect.x += dx
+        spieler.rect.y += dy
+
+
+    def gegner_bewegen(self, g_links, g_rechts):
+        dx = 0
+        dy = 0
+
+        if gegner2.sprung == True and gegner2.in_luft == False:
+            gegner2.s_hoehe_y = -15
+            gegner2.sprung = False
+            gegner2.in_luft = True
+
+        #Gravitation einfügen
+        gegner2.s_hoehe_y += GRAVITATION
+        if gegner2.s_hoehe_y > 15:
+            gegner2.s_hoehe_y
+        dy += gegner2.s_hoehe_y
+
+        #Kollision checken mit Boden checken
+        if gegner2.rect.bottom + dy > 500:
+            dy = 500 - gegner2.rect.bottom
+            gegner2.in_luft = False
+
+        if gegner2.angriff == True and gegner2.bild_index == 5:
+            gegner2.angriff = False
+
+        if g_links:
+            dx = -gegner2.speed
+            gegner2.drehen = True
+            gegner2.richtung = -1
+        if g_rechts:
+            dx = gegner2.speed
+            gegner2.drehen = False
+            gegner2.richtung = 1
+
+        gegner2.rect.x += dx
+        gegner2.rect.y += dy
 
 
 
@@ -134,16 +174,10 @@ class Figur(pygame.sprite.Sprite):
                 self.rect.y = -300
 
 
-gegner_gruppe = pygame.sprite.Group()
-
 spieler = Figur('HeroKnight', 200, 500, 2, 5)
-gegner1 = Figur('Gegner', 200, 443, 0.2, 5)
-gegner2 = Figur('Gegner', 600, 443, 0.2, 5)
 
+gegner2 = Figur('Gegner', 600, 500, 0.2, 5)
 
-gegner_liste = []
-gegner_liste.append(gegner1)
-gegner_liste.append(gegner2)
 
 run = True
 while run:
@@ -152,34 +186,55 @@ while run:
     draw_bg()
 
     spieler.update_animation()
-    gegner1.update_animation()
+
     gegner2.update_animation()
     spieler.draw()
-    gegner1.draw()
+
     gegner2.draw()
-    gegner1.tod()
+
+    spieler.tod()
     gegner2.tod()
+
 
     #Spieler Aktion Auswahl
     if spieler.lebendig:
         if spieler.in_luft:
             spieler.update_aktion(2)#2: springen
-            #gegner.update_aktion(2)
+            #gegner1.update_aktion(0)
         elif l_links or l_rechts:
             spieler.update_aktion(1)#1: rennen
-            #gegner.update_aktion(1)
+            #gegner1.update_aktion(1)
         elif spieler.angriff:
             spieler.update_aktion(3)
-            #gegner.update_aktion(3)
+            #gegner1.update_aktion(3)
         else:
             spieler.update_aktion(0)#0: stehen
-            gegner1.update_aktion(0)
-            gegner2.update_aktion(0)
-        spieler.bewegen(l_links, l_rechts)
+        spieler.spieler_bewegen(l_links, l_rechts)
 
-    for gegner in gegner_liste:
-        if gegner.lebendig == False:
-            gegner.update_aktion(4)
+    if gegner2.lebendig:
+        if gegner2.in_luft:
+            gegner2.update_aktion(2)  # 2: springen
+        elif g_links or g_rechts:
+            gegner2.update_aktion(1)  # 1: rennen
+            # gegner1.update_aktion(1)
+        elif gegner2.angriff:
+            gegner2.update_aktion(3)
+        else:
+            gegner2.update_aktion(0)
+        gegner2.gegner_bewegen(g_links, g_rechts)
+
+    if gegner2.lebendig == False:
+            gegner2.update_aktion(4)
+            if gegner2.bild_index == 4:
+                hintergrund = pygame.image.load(
+                    "Bilder/Hintergrund/parallax_mountain_pack/layers/hi.jpg").convert()
+
+    if spieler.lebendig == False:
+            spieler.update_aktion(4)
+            if spieler.bild_index == 4:
+                hintergrund = pygame.image.load(
+                    "Bilder/Hintergrund/parallax_mountain_pack/layers/hi.jpg").convert()
+
 
 
     for event in pygame.event.get():
@@ -192,17 +247,32 @@ while run:
                 l_links = True
             if event.key == pygame.K_d:
                 l_rechts = True
+            if event.key == pygame.K_LEFT:
+                g_links = True
+            if event.key == pygame.K_RIGHT:
+                g_rechts = True
             if event.key == pygame.K_w and spieler.lebendig:
                 spieler.sprung = True
+            if event.key == pygame.K_UP and gegner2.lebendig:
+                gegner2.sprung = True
             if event.key == pygame.K_SPACE:
                 spieler.angriff = True
-                for gegner in gegner_liste:
-                    if pygame.sprite.collide_rect(spieler, gegner):
-                        if gegner.lebendig:
-                            gegner.leben -= 25
-                            print(gegner.leben)
+                if pygame.sprite.collide_rect(spieler, gegner2):
+                    if gegner2.lebendig:
+                        gegner2.leben -= 25
+                        print(gegner2.leben)
+            if event.key == pygame.K_RSHIFT:
+                gegner2.angriff = True
+                if pygame.sprite.collide_rect(gegner2, spieler):
+                    if spieler.lebendig:
+                        spieler.leben -= 25
+                        print(spieler.leben)
             if event.key == pygame.K_ESCAPE:
                 run = False
+            if event.key == pygame.K_RETURN and spieler.lebendig == False:
+                os.execl(sys.executable, sys.executable, *sys.argv)
+            if event.key == pygame.K_RETURN and gegner2.lebendig == False:
+                os.execl(sys.executable, sys.executable, *sys.argv)
 
         #keyboard button released
         if event.type == pygame.KEYUP:
@@ -210,7 +280,10 @@ while run:
                 l_links = False
             if event.key == pygame.K_d:
                 l_rechts = False
-
+            if event.key == pygame.K_LEFT:
+                g_links = False
+            if event.key == pygame.K_RIGHT:
+                g_rechts = False
 
 
 
